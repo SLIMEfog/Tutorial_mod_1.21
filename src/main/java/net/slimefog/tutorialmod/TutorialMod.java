4e3ab6ec449cda1a560eb1d239f021d1aea16e7e
SLIMEfog <woodcutter09@proton.me>
2026-01-25 15:33:26 -0500

    Finished Part 2

--- src/main/java/net/slimefog/tutorialmod/TutorialMod.java
+++ src/main/java/net/slimefog/tutorialmod/TutorialMod.java
@@ -2,6 +2,8 @@ package net.slimefog.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.slimefog.tutorialmod.block.ModBlocks;
+import net.slimefog.tutorialmod.item.ModItemGroups;
 import net.slimefog.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,5 +15,7 @@ public class TutorialMod implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
+		ModItemGroups.registerItemGroups();
 	}
 }
