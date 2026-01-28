42a86e2b18f2afbc8243e5926b3102a8566e6cdf
SLIMEfog <woodcutter09@proton.me>
2026-01-28 12:36:44 -0500

    Finished Part 11

---
+++ src/main/java/net/slimefog/tutorialmod/datagen/ModItemTagProvider.java
@@ -0,0 +1,25 @@
+package net.slimefog.tutorialmod.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
+import net.minecraft.registry.RegistryWrapper;
+import net.slimefog.tutorialmod.item.ModItems;
+import net.slimefog.tutorialmod.util.ModTags;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ModItemTagProvider extends FabricTagProvider.ItemTagProvider {
+    public ModItemTagProvider(FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> completableFuture) {
+        super(output, completableFuture);
+    }
+
+    @Override
+    protected void configure(RegistryWrapper.WrapperLookup wrapperLookup) {
+        getOrCreateTagBuilder(ModTags.Items.TRANSFORMABLE_ITEMS)
+                .add(ModItems.PINK_GARNET)
+                .add(ModItems.RAW_PINK_GARNET)
+                .add(ModItems.STARLIGHT_ASHES)
+                .add(ModItems.CAULIFLOWER)
+                .add(ModItems.CHISEL);
+    }
+}
